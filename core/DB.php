2094a5b7652409208201fc9be01dae16bbd8b1bd
Jay Prakash <jp024556@gmail.com>
2018-09-12 12:33:38 +0530

    DB Customized Error Handling Added

--- core/DB.php
+++ core/DB.php
@@ -6,9 +6,8 @@ class DB{
 	public $conn;
 
 	private function __construct(){
-		$this->conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE);
-		if(!$this->conn){
-			return mysqli_connect_error();
+		if(@!$this->conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE)){
+			die("<div style='background:red;padding:20px;width:50%;margin:5px auto;font-family:Verdana;color:#fff;box-shadow:0px 3px 10px #ccc;font-size:17px;'><b>Geeky Warning(DB): </b>".mysqli_connect_error()."</div>");
 		}
 	}
 
